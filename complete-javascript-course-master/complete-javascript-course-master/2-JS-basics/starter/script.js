0a9db28b2574223430458654d809b369e0061b0c
Anand-09 <prem.anand80@yahoo.com>
2020-08-30 05:59:55 +0530

    Folder Restructuring

---
+++ complete-javascript-course-master/complete-javascript-course-master/2-JS-basics/starter/script.js
@@ -0,0 +1,31 @@
+/************************* 
+*Variables & Data Types  
+*/
+
+console.log('Hello World!!!');
+
+var firstName = 'John';
+console.log(firstName);
+
+var lastName = 'Smith';
+console.log(lastName);
+
+var age = 28;
+console.log(age);
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+job = 'teacher';
+console.log(job);
+
+// Variable naming rues
+var _3years = 3;
+var joyhn = 'John amd Mark';
+
+/************************* 
+*Variables mutation & type coercion 
+*/
